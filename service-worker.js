5032eaa0503c376c33a8fac06a930f6c8acf8a0d
Md Wira Putra Dananjaya <wira.putra03.wp@gmail.com>
2024-11-12 22:49:29 +0800

    wad

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,37 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = "my-pwa-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/service-worker.js',
-  '/manifest.json',
-  '/icon.png'
+  "/",
+  "/index.html",
+  "/service-worker.js",
+  "/manifest.json",
+  "/icon.png",
 ];
 
 // Install service worker
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches
+      .open(CACHE_NAME)
       .then((cache) => {
         return cache.addAll(urlsToCache);
       })
-  ); 
-});
-
-// Fetch cached files
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
+      .catch((error) => {
+        console.error("Gagal menambahkan sumber daya ke cache:", error);
       })
   );
 });
 
+// Fetch cached files
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 // Update cache
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
